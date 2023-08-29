4f66b65663d670363c3213a6353b51219d440066
imad bouyfri <imad.bouyfri@outlook.fr>
2023-08-29 01:26:04 +0100

    update safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
-            print(my_list[i], end='')
-            count += 1
-        print()
-        return count
-    except IndexError:
-        print()
-        return count
+    counter = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            counter += 1
+        except IndexError:
+            pass
+    print("")
+    return counter
