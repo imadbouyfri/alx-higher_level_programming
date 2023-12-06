5d6c79a57dfd48946b7537787e657a6d62f1c1a1
imad bouyfri <imad.bouyfri@outlook.fr>
2023-12-06 02:50:41 +0100

    update

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # makes a request to [::-1]/catch_me that causes the server to respond with a message
-curl -s 0.0.0..0:5000/catch_me -X PUT -L -d "user_id=98" -H "Origin: School"
+curl -sX PUT 54.160.95.240:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
+
